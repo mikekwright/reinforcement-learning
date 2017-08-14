8ce23801121dfff33510fa6caf02263c2709574b
Michael Wright <mkwright@gmail.com>
2017-08-13 22:34:37 -0600

    Going through the tutorial for OpenAI Gym (cartpole)

--- 00-tutorial/cart_pole.py
+++ 00-tutorial/cart_pole.py
@@ -2,23 +2,80 @@ import gym
 import os
 import shutil
 
+from collections import defaultdict
+from pprint import pprint
+
 
 class CartPole:
-    def __init__(self):
+    def __init__(self, alpha=0.1, max_count=10000):
+        self._alpha = alpha
+        self._max_count = max_count
         self._env = None
+        self._complete = False
+        self._last_state = None
+        self._state_values = None
 
     def init(self, overwrite=False):
-        if not overwrite and os.path.exists(os.path.join(os.path.dirname(__file__), 'pole-results')):
-            raise ValueError('You need to first remove the previous results or run with "--overwrite"')
+        results_path = os.path.join(os.path.dirname(__file__), 'pole-results')
+        if not overwrite and os.path.exists(results_path):
+            raise ValueError('You need to first remove the previous results, use "--overwrite"')
 
         if overwrite:
-            shutil.rmtree(os.path.join(os.path.dirname(__file__), 'pole-results'))
+            shutil.rmtree(results_path)
 
-        self._env = gym.wrappers.Monitor(gym.make('CartPole-v0'), 'pole-results')
+        self._env = gym.wrappers.Monitor(gym.make('CartPole-v0'), results_path)
+        self._state_values = defaultdict(lambda: 0)
 
-    def start(self):
-        self._env.reset()
+    @staticmethod
+    def _state_from_observation(observation, action=-1):
+        return (tuple(int(i * 100) for i in observation), action)
 
-    def play_game(self):
-        self._env.render()
-        self._env.step(self._env.action_space.sample())
+    def run_episode(self):
+        """
+        Run an e
+
+        :param param1:
+        :returns:
+        :raises keyError:
+        """
+        current_observation = self._env.reset()
+        current_state = self._state_from_observation(current_observation)
+        self._complete = False
+
+        done = False
+        count = 0
+        while not done and count < self._max_count:
+            self._env.render()
+            available_actions = tuple(range(self._env.action_space.n))
+
+            max_move = None
+            for a in available_actions:
+                key = self._state_from_observation(current_observation, a)
+                if max_move is None or self._state_values[max_move] < self._state_values[key]:
+                    max_move = (key, a)
+
+            selected_action = max_move[1]
+            # observation, reward, done, info = self._env.step(self._env.action_space.sample())
+            observation, reward, done, _ = self._env.step(selected_action)
+            next_state = self._state_from_observation(observation, selected_action)
+            self._state_values[current_state] += self._alpha * (reward + self._state_values[next_state])
+            current_state = next_state
+            count += 1
+
+        print(f'Episode complete: count {count}')
+        self._state_values[current_state] = 0
+
+        # for i, state in enumerate(visited_states):
+        #     self._state_values[state] += self._alpha * (count - self._state_values[state])
+            # self._state_values[max_move] += self._alpha * (reward - self._state_values[max_move])
+            # print(observation, reward, done)
+
+        if self._max_count == count:
+            self._complete = True
+
+    @property
+    def complete(self):
+        return self._complete
+
+    def display_results(self):
+        pprint(self._state_values)
--- 00-tutorial/main.py
+++ 00-tutorial/main.py
@@ -6,15 +6,24 @@ from cart_pole import CartPole
 @click.command()
 @click.argument('episodes', type=int)
 @click.option('--overwrite', is_flag=True, default=False, help='Remove previous results')
-def train(episodes, overwrite):
+@click.option('--alpha', default=0.1, type=float, help='The alpha for the model')
+def train(episodes, overwrite, alpha):
 
-    pole = CartPole()
+    pole = CartPole(alpha=alpha)
     pole.init(overwrite=overwrite)
 
-    pole.start()
-
     for _ in range(episodes):
-        pole.play_game()
+        pole.run_episode()
+
+        if pole.complete:
+            break
+
+    pole.display_results()
+
+    if pole.complete:
+        print('Congrats, you got it')
+    else:
+        print(f'Unable to find the solution in {episodes} episodes')
 
 
 if __name__ == "__main__":
